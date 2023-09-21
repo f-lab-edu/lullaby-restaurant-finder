44155f23a68b86b1a244384428eedb8a8d48a24c
lullaaaby13 <sds901234@naver.com>
2023-09-21 13:12:08 +0900

    feat: 프로젝트 생성

---
+++ src/main/kotlin/com/lullaby/flab/restrauntfinderapi/RestrauntFinderApiApplication.kt
@@ -0,0 +1,11 @@
+package com.lullaby.flab.restrauntfinderapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestrauntFinderApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestrauntFinderApiApplication>(*args)
+}
