5c92dc2ce1f673a533af29b5dd6e751e0761511b
lullaaaby13 <sds901234@naver.com>
2023-09-23 17:47:27 +0900

    feat: Bearer 토큰에 대해 매직 넘버 도입

--- src/main/kotlin/com/lullaby/flab/restrauntfinderapi/security/JwtFilter.kt
+++ src/main/kotlin/com/lullaby/flab/restrauntfinderapi/security/JwtFilter.kt
@@ -28,11 +28,13 @@ class JwtFilter(
 
     private fun resolveToken(request: HttpServletRequest): String? {
         val bearerToken = request.getHeader(AUTHORIZATION_HEADER) ?: return null
-        return if (bearerToken.startsWith("Bearer ")) bearerToken.substring(7) else null
+        return if (bearerToken.startsWith(BEARER_TOKEN_PREFIX)) bearerToken.substring(BEARER_TOKEN_LENGTH) else null
     }
 
     companion object {
         const val AUTHORIZATION_HEADER = "Authorization"
+        const val BEARER_TOKEN_PREFIX = "Bearer "
+        const val BEARER_TOKEN_LENGTH = BEARER_TOKEN_PREFIX.length
     }
 
 }
