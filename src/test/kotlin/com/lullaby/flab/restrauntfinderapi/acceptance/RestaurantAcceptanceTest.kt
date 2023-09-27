43dab3ee4e7cc6fb78c2d392cd828d6400eed837
lullaaaby13 <sds901234@naver.com>
2023-09-27 16:03:36 +0900

    test: 식당 조회 인수 테스트 추가

--- src/test/kotlin/com/lullaby/flab/restrauntfinderapi/acceptance/RestaurantAcceptanceTest.kt
+++ src/test/kotlin/com/lullaby/flab/restrauntfinderapi/acceptance/RestaurantAcceptanceTest.kt
@@ -3,15 +3,9 @@ package com.lullaby.flab.restrauntfinderapi.acceptance
 import com.lullaby.flab.restrauntfinderapi.acceptance.fixture.로그인
 import com.lullaby.flab.restrauntfinderapi.acceptance.fixture.식당_생성
 import com.lullaby.flab.restrauntfinderapi.acceptance.fixture.회원_가입
-import com.lullaby.flab.restrauntfinderapi.application.auth.request.SignInRequest
-import com.lullaby.flab.restrauntfinderapi.application.auth.request.SignUpRequest
-import com.lullaby.flab.restrauntfinderapi.application.restaurant.command.CreateRestaurantCommand
 import com.lullaby.flab.restrauntfinderapi.application.restaurant.response.RestaurantResponse
 import com.lullaby.flab.restrauntfinderapi.domain.FoodType
 import io.restassured.RestAssured
-import io.restassured.builder.RequestSpecBuilder
-import io.restassured.specification.RequestSpecification
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -34,6 +28,24 @@ class RestaurantAcceptanceTest : AcceptanceTest() {
     fun create() {
         val restaurantResponse = 식당_생성(accessToken!!, "강남교자 본점", "서울 서초구 강남대로69길 11 삼미빌딩", 20, FoodType.KOREAN)
         assertThat(restaurantResponse.id).isNotNull
+    }
+
+    @Test
+    fun lists() {
+        식당_생성(accessToken!!, "강남교자 본점", "서울 서초구 강남대로69길 11 삼미빌딩", 20, FoodType.KOREAN)
+        식당_생성(accessToken!!, "쉐이크쉑 강남대로점", "서울 서초구 강남대로 421", 30, FoodType.WESTERN)
+
+        val response = RestAssured
+            .given().log().all()
+            .header("Authorization", "Bearer $accessToken")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .`when`().get("/restaurants")
+            .then().log().all().extract()
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
+
+        val restaurantResponses = response.jsonPath().getList("", RestaurantResponse::class.java)
+        assertThat(restaurantResponses.size).isEqualTo(2)
     }
 
 }
