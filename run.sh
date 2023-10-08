014167494aaf22b9b08b5b57e997cbcc0afc08f3
lullaaaby13 <sds901234@naver.com>
2023-10-08 18:36:50 +0900

    chore: 불필요한 파일 삭제

--- run.sh
+++
@@ -1,7 +0,0 @@
-java -jar -javaagent:./pinpoint-agent/pinpoint-bootstrap-2.5.2.jar \
-  -Dpinpoint.applicationName=restaurant-finder-api \
-  -Dpinpoint.agentId=restaurant-finder-api \
-  -Dpinpoint.agentName=restaurant-finder-api-local-agent \
-  -Dpinpoint.config=./pinpoint-agent/pinpoint-root.config \
-  -Dspring.profiles.active=local \
-  ./build/libs/app.jar
