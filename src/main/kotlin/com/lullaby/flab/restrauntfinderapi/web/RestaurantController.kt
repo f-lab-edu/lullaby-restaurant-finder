50a1f5bba42c30cbee53712a8f1daa1f122666a1
lullaaaby13 <sds901234@naver.com>
2023-10-10 14:03:11 +0900

    feat: 메뉴 추가 API 추가

--- src/main/kotlin/com/lullaby/flab/restrauntfinderapi/web/RestaurantController.kt
+++ src/main/kotlin/com/lullaby/flab/restrauntfinderapi/web/RestaurantController.kt
@@ -1,10 +1,12 @@
 package com.lullaby.flab.restrauntfinderapi.web
 
 import com.lullaby.flab.restrauntfinderapi.application.restaurant.RestaurantService
+import com.lullaby.flab.restrauntfinderapi.application.restaurant.command.CreateMenuCommand
 import com.lullaby.flab.restrauntfinderapi.application.restaurant.command.CreateRestaurantCommand
 import com.lullaby.flab.restrauntfinderapi.application.restaurant.response.RestaurantResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -22,5 +24,13 @@ class RestaurantController(
 
     @GetMapping("/restaurants")
     fun listRestaurant(): List<RestaurantResponse> = restaurantService.list()
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/restaurants/{restaurantId}/menus")
+    fun createMenu(
+        @PathVariable restaurantId: Long,
+        @RequestBody request: CreateMenuCommand
+    ): RestaurantResponse
+        = restaurantService.createMenu(restaurantId, request)
 
 }
