e933863ac8306ab46a8036181910dfde028da255
lullaaaby13 <sds901234@naver.com>
2023-10-10 13:13:47 +0900

    test: 메뉴 생성 인수 테스트 추가

--- src/test/kotlin/com/lullaby/flab/restrauntfinderapi/acceptance/RestaurantAcceptanceTest.kt
+++ src/test/kotlin/com/lullaby/flab/restrauntfinderapi/acceptance/RestaurantAcceptanceTest.kt
@@ -4,9 +4,11 @@ import com.lullaby.flab.restrauntfinderapi.acceptance.fixture.로그인
 import com.lullaby.flab.restrauntfinderapi.acceptance.fixture.식당_생성
 import com.lullaby.flab.restrauntfinderapi.acceptance.fixture.식당_조회
 import com.lullaby.flab.restrauntfinderapi.acceptance.fixture.회원_가입
+import com.lullaby.flab.restrauntfinderapi.application.restaurant.command.CreateRestaurantCommand
 import com.lullaby.flab.restrauntfinderapi.application.restaurant.response.RestaurantResponse
 import com.lullaby.flab.restrauntfinderapi.domain.FoodType
 import io.restassured.RestAssured
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -39,6 +41,34 @@ class RestaurantAcceptanceTest : AcceptanceTest() {
 
         val restaurantResponses = 식당_조회(accessToken!!)
         assertThat(restaurantResponses.size).isEqualTo(2)
+    }
+
+    @DisplayName("식당에 메뉴를 추가 한다.")
+    @Test
+    fun createMenu() {
+        val restaurantResponse = 식당_생성(
+            accessToken!!,
+            "강남교자 본점",
+            "서울 서초구 강남대로69길 11 삼미빌딩",
+            20,
+            FoodType.KOREAN
+        )
+
+        val command = mapOf(
+            "name" to "교자 칼국수",
+            "price" to "11000",
+            "type" to "MAIN"
+        )
+
+        val response = RestAssured
+            .given().log().all()
+            .header("Authorization", "Bearer $accessToken")
+            .body(command)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .`when`().post("/restaurants/${restaurantResponse.id}/menus")
+            .then().log().all().extract()
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
     }
 
 }
